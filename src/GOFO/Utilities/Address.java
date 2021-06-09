4d5c7c0078248f347ac127424fff8477484326ed
Muhammad1995-djiddo <djiddo23@gmail.com>
2021-06-09 09:49:27 +0200

    Implemented Address.java

--- src/GOFO/Utilities/Address.java
+++ src/GOFO/Utilities/Address.java
@@ -1,7 +1,74 @@
 package GOFO.Utilities;
 
 public class Address {
-    public Address(int streetNumber, String streetName, String city) {
+    private int streetNumber;
+    private String streetName;
+    private String city;
 
+    /**
+     * Constructs an Address object which has:
+     * <p>A street number, A street name, A city name
+     * @param streetNumber street number
+     * @param streetName street name
+     * @param city city name
+     */
+    public Address(int streetNumber, String streetName, String city) {
+        setStreetNumber(streetNumber);
+        setStreetName(streetName);
+        setCity(city);
+    }
+
+    /**
+     * Set street number
+     * @param streetNumber street number
+     */
+    public void setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    /**
+     * Set street name
+     * @param streetName street name
+     */
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    /**
+     * Set city
+     * @param city city
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * Returns street number
+     * @return street number
+     */
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    /**
+     * Returns street name
+     * @return street name
+     */
+    public String getStreetName() {
+        return streetName;
+    }
+
+    /**
+     * Returns city
+     * @return city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public String toString() {
+        return "Address: " + getStreetNumber()
+                + " " + getStreetName() + " " + getCity();
     }
 }
